92134d8aab4ce5539eec3969a8a877c7920ee926
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-02-06 19:16:37 -0800

    Add player data type

---
+++ src/types/DataTypes.ts
@@ -0,0 +1,9 @@
+export interface Player {
+    country: string | null;
+    id: string | null;
+    playHand: string | null;
+    birthDate: string | null;
+    name: string | null;
+    event: string | null;
+    height: number | null;
+}
