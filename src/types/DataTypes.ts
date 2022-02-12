4ba443c089e7077f63757c29d709b7dbe1c00e7b
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-02-12 01:11:31 -0800

    Add util fetch functions

--- src/types/DataTypes.ts
+++ src/types/DataTypes.ts
@@ -1,9 +1,9 @@
 export interface Player {
+    name: string;
+    event: string;
     country: string | null;
     id: string | null;
     playHand: string | null;
     birthDate: string | null;
-    name: string | null;
-    event: string | null;
     height: number | null;
 }
---
+++ src/utils/SearchUtils.tsx
@@ -0,0 +1,53 @@
+const SEARCH_LIMIT = 25;
+
+export const fetchSuggestions = async (text: string): Promise<any> => {
+  text.replace(" ", "%20");
+  return fetch(
+    "https://badminton-api.com/player/search?name=" + text + "&limit=" + SEARCH_LIMIT
+  )
+    .then(
+      (res) => {
+        return res.json();
+      },
+      (err) => {
+        console.log(err);
+        return Promise.reject(err);
+      }
+    )
+    .then(
+      (result) => {
+        console.log(result);
+        return Promise.resolve(result.data);
+      },
+      (error) => {
+        console.log(error);
+        return Promise.reject(error);
+      }
+    );
+};
+
+export const fetchPlayer = async (uid: string): Promise<any> => {
+    return fetch(
+      "https://badminton-api.com/player?player_id=" + uid
+    )
+      .then(
+        (res) => {
+          return res.json();
+        },
+        (err) => {
+          console.log(err);
+          return Promise.reject(err);
+        }
+      )
+      .then(
+        (result) => {
+          console.log(result);
+          return Promise.resolve(result.data);
+        },
+        (error) => {
+          console.log(error);
+          return Promise.reject(error);
+        }
+      );
+  };
+  
