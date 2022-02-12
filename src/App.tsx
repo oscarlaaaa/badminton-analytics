04eb939848d08b57b0441a7c47021b9fccd184d0
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-02-12 01:10:26 -0800

    Change path hierarchy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/landing/LandingPage";
 import SearchPage from "./components/search/SearchPage";
 import DashboardPage from "./components/dashboard/DashboardPage";
 
@@ -9,8 +8,7 @@ const App: React.FC = (): React.ReactElement => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LandingPage />} />
-        <Route path="/search" element={<SearchPage />} />
+        <Route index element={<SearchPage />} />
         <Route path="/dashboard/:uid" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
