dcded81b18a7491cff635648b2852ce5f5797082
Oscar La <67846380+oscarlaaaa@users.noreply.github.com>
2022-02-15 22:26:53 -0800

    Add more datatypes to model API return data

--- src/types/DataTypes.ts
+++ src/types/DataTypes.ts
@@ -1,9 +1,33 @@
 export interface Player {
-    name: string;
-    event: string;
-    country: string | null;
-    id: string | null;
-    playHand: string | null;
-    birthDate: string | null;
-    height: number | null;
+  name: string;
+  event: string;
+  country: string | null;
+  id: string | null;
+  playHand: string | null;
+  birthDate: string | null;
+  height: number | null;
+}
+
+export interface Match {
+  winnerId: string;
+  loserId: string;
+  tournamentId: string;
+  duration: number | null;
+}
+
+export interface Set {
+  winnerId: string;
+  loserId: string;
+  tournamentId: string;
+  duration: number | null;
+  round: 1;
+  winnerScore: number | null;
+  loserScore: number | null;
+}
+
+export interface Tournament {
+  name: string;
+  startDate: string;
+  endDate: string;
+  id: string;
 }
