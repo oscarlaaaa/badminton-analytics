5c9c7995a4143298b39a7394ea63b91d6c5cc340
Oscar <oscarla5747@gmail.com>
2022-02-25 21:36:06 -0800

    General refactoring

---
+++ src/components/dashboard/player_info/PlayerGeneralInfo.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { Player } from "../../../types/DataTypes";
+
+interface PlayerSuggestionProps {
+  player: Player;
+}
+
+const PlayerGeneralInfo: React.FC<PlayerSuggestionProps> = ({ player }) => {
+  return (
+    <div>
+      <h1>{player.name}</h1>
+      <ul>
+        <li>
+          <b>ID:</b> {player.id ?? "N/A"}
+        </li>
+        <li>
+          <b>Name: </b>
+          {player.name ?? "N/A"}
+        </li>
+        <li>
+          <b>Country: </b>
+          {player.country ?? "N/A"}
+        </li>
+        <li>
+          <b>Event: </b>
+          {player.event ?? "N/A"}
+        </li>
+        <li>
+          <b>BirthDate: </b>
+          {player.birthDate ?? "N/A"}
+        </li>
+        <li>
+          <b>Playing Hand: </b>
+          {player.playHand ?? "N/A"}
+        </li>
+        <li>
+          <b>Height: </b>
+          {player.height ? player.height + "cm" : "N/A"}{" "}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default PlayerGeneralInfo;
