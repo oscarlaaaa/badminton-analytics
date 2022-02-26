87c4e86dee65d4df48604a684b8a6ba0bd3cee19
Oscar <oscarla5747@gmail.com>
2022-02-25 21:34:57 -0800

    General refactoring

--- src/components/dashboard/DashboardPage.tsx
+++ src/components/dashboard/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
 import * as SearchUtils from "../../utils/SearchUtils";
-import PlayerGeneralInfo from "./PlayerGeneralInfo";
+import PlayerGeneralInfo from "./player_info/PlayerGeneralInfo";
 import { Player } from "../../types/DataTypes";
 import MatchHistoryContainer from "./matches/MatchHistoryContainer";
 
@@ -17,10 +17,7 @@ const DashboardPage: React.FC = (): React.ReactElement => {
   }, []);
   return player ? (
     <div>
-      <h1>{player.name}</h1>
       <PlayerGeneralInfo player={player} />
-      <br />
-      <h2>Match History</h2>
       <MatchHistoryContainer player={player} />
     </div>
   ) : (
