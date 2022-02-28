2ae1e69096be3d72e572ea7de10eb5717cacf718
Oscar <oscarla5747@gmail.com>
2022-02-27 16:30:36 -0800

    Adjust page layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchPage from "./components/search/SearchPage";
 import DashboardPage from "./components/dashboard/DashboardPage";
+import './styles/App.css';
 
 const App: React.FC = (): React.ReactElement => {
   return (
--- src/components/search/SearchPage.tsx
+++ src/components/search/SearchPage.tsx
@@ -5,7 +5,7 @@ import SearchBarContainer from "./search_bar/SearchBarContainer";
 import SearchResultsContainer from "./search_results/SearchResultsContainer";
 import TopPlayersContainer from "./top_players/TopPlayersContainer";
 
-import { Skeleton, Paper } from "@mantine/core";
+import { Skeleton, Center, Paper, Grid } from "@mantine/core";
 
 const SearchPage: React.FC = (): React.ReactElement => {
   const [text, setText] = React.useState<string>("");
@@ -13,6 +13,7 @@ const SearchPage: React.FC = (): React.ReactElement => {
   const [suggestions, setSuggestions] = React.useState<Player[]>(null);
   const [noSuggestions, setNoSuggestions] = React.useState<string>(null);
   const [loadingError, setLoadingError] = React.useState<boolean>(false);
+  const [firstClick, setFirstClick] = React.useState<boolean>(false);
 
   const onTextChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setText(event.target.value);
@@ -20,6 +21,7 @@ const SearchPage: React.FC = (): React.ReactElement => {
 
   const onSearch = (): void => {
     setIsLoading(true);
+    setFirstClick(true);
     SearchUtils.fetchSuggestions(text).then(
       (suggest) => {
         setSuggestions(suggest ?? null);
@@ -35,52 +37,64 @@ const SearchPage: React.FC = (): React.ReactElement => {
   };
 
   return (
-    <div
+    <Center
       style={{
         textAlign: "center",
-        width: "90vw",
-        height: "90vh",
-        margin: "4vh 4vw 4vh 4vw",
+        width: "97vw",
+        height: "97vh",
       }}
     >
-      <h1>Badminton Analytics Dashboard</h1>
-      <p style={{ fontSize: "14pt" }}>
-        Built using TypeScript, React, Redux (soon), AWS Lambda (soon), Heroku,
-        and the{" "}
-        <a href="https://api.badminton-api.com">Badminton Singles API</a>
-      </p>
-
-      <SearchBarContainer
-        text={text}
-        onTextChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          onTextChange(event)
-        }
-        onSearch={() => onSearch()}
-      />
-      <Skeleton height={isLoading ? "200px" : "auto"} visible={isLoading}>
-        <SearchResultsContainer
-          noSuggestions={noSuggestions}
-          suggestions={suggestions}
-        />
-      </Skeleton>
-      {!suggestions && (
-        <div
+      <div
+        id="search-area"
+        style={{ height: "100%", margin: "3%", width: "70%" }}
+      >
+        <h1
           style={{
-            display: "flex",
-            margin: "5%",
-            justifyContent: "space-evenly",
+            marginTop: "6%",
+            marginBottom: "0",
+            textAlign: "left",
+            fontSize: "58pt",
           }}
         >
-          <Paper padding="xl" shadow="l">
-            <TopPlayersContainer event={"MS"} count={10} />
+          Badminton <br />
+          Analytics Dashboard
+        </h1>
+        <p style={{ fontSize: "14pt", textAlign: "left" }}>
+          Built using TypeScript, React, Python, AWS Lambda (soon), Heroku, and
+          the <a href="https://api.badminton-api.com">Badminton Singles API</a>.
+        </p>
+        <SearchBarContainer
+          text={text}
+          onTextChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            onTextChange(event)
+          }
+          onSearch={() => onSearch()}
+        />
+        {firstClick && (
+          <Skeleton height={isLoading ? "200px" : "auto"} visible={isLoading}>
+            <SearchResultsContainer
+              noSuggestions={noSuggestions}
+              suggestions={suggestions}
+            />
+          </Skeleton>
+        )}
+      </div>
+      <div style={{ width: "30%" }}>
+        <div
+          style={{
+            margin: "5%",
+          }}
+        >
+          <Paper padding="sm" shadow="l" m="md">
+            <TopPlayersContainer event={"MS"} count={5} />
           </Paper>
-          <Paper padding="xl" shadow="l">
-            <TopPlayersContainer event={"WS"} count={10} />
+          <Paper padding="sm" shadow="l" m="md">
+            <TopPlayersContainer event={"WS"} count={5} />
           </Paper>
         </div>
-      )}
+      </div>
       {loadingError && <p>Sorry, there was an error. Please try again.</p>}
-    </div>
+    </Center>
   );
 };
 
