b0ad460fd6ff7c1491ba6f800567b9028d6d0c08
Oscar <oscarla5747@gmail.com>
2022-04-05 19:47:59 -0700

    Add 1337 stats

--- src/components/dashboard/cool_stats/CoolStatsComponent.tsx
+++ src/components/dashboard/cool_stats/CoolStatsComponent.tsx
@@ -1,13 +1,21 @@
 import { Container } from "@mantine/core";
 import * as React from "react";
 import { useMediaQuery } from '@mantine/hooks';
+import { CoolStats } from "../../../types/OtherTypes";
 
-const CoolStatsComponent = () => {
+interface CoolStatsProps {
+  coolStats: CoolStats;
+}
+
+const CoolStatsComponent: React.FC<CoolStatsProps> = ({ coolStats }) => {
+  React.useEffect(() => {
+    console.log(coolStats);
+  }, [])
   const largeScreen = useMediaQuery('(min-width: 1280px)');
   return (
     <Container
       fluid
-      style={{width: largeScreen ? "46%" : "96%", margin: "10px"}}
+      style={{width: largeScreen ? "47%" : "98%", margin: largeScreen ? "5px auto 5px 0" : "5px auto"}}
     >
       heh
     </Container>
--- src/components/dashboard/cool_stats/CoolStatsContainer.tsx
+++ src/components/dashboard/cool_stats/CoolStatsContainer.tsx
@@ -1,12 +1,64 @@
 import * as React from "react";
+import { Match, Player } from "../../../types/DataTypes";
+import { CoolStats } from "../../../types/OtherTypes";
 import CoolStatsComponent from "./CoolStatsComponent";
 
 interface CoolStatsProps {
-  player: string;
+  player: Player;
+  matches: Match[];
 }
-const CoolStatsContainer: React.FC<CoolStatsProps> = ({ player }) => {
 
-  return <CoolStatsComponent></CoolStatsComponent>;
+function processCoolStats(player: Player, matches: Match[]): CoolStats {
+  let [longestWinStreak, longestLossStreak] = [0, 0];
+  let [currentLongestWinStreak, currentLongestLossStreak] = [0, 0];
+  let [totalPointsWon, totalPointsLost, totalMatchesWon, totalMatchesLost, totalMatchDuration] = [0, 0, 0, 0, 0];
+  for (let match of matches) {
+    totalMatchDuration += match.duration;
+    totalPointsWon += match.winnerId === player.id ? match.winnerPoints : match.loserPoints;
+    totalPointsLost += match.loserId === player.id ? match.winnerPoints : match.loserPoints;
+    if (match.winnerId === player.id) {
+      totalMatchesWon++;
+    } else {
+      totalMatchesLost++;
+    }
+
+    if (match.winnerId === player.id) {
+      currentLongestWinStreak++;
+      currentLongestLossStreak = 0;
+    } else {
+      currentLongestLossStreak++;
+      currentLongestWinStreak = 0;
+    }
+    
+    if (currentLongestLossStreak > longestLossStreak) {
+      longestLossStreak = currentLongestLossStreak;
+    }
+    if (currentLongestWinStreak > longestWinStreak) {
+      longestWinStreak = currentLongestWinStreak;
+    }
+  }
+
+  const coolStats: CoolStats = {
+    longestWinStreak: longestWinStreak,
+    longestLossStreak: longestLossStreak,
+    totalPointsWon: totalPointsWon,
+    totalPointsLost: totalPointsLost,
+    totalMatchesWon: totalMatchesWon,
+    totalMatchesLost: totalMatchesLost,
+    totalMatchDuration: totalMatchDuration,
+    totalNumOfMatches: matches.length
+  }
+
+  return coolStats;
+}
+
+const CoolStatsContainer: React.FC<CoolStatsProps> = ({ player, matches }) => {
+  const [coolStats, setCoolStats] = React.useState<CoolStats | null>(null);
+  React.useEffect(() => {
+    setCoolStats(processCoolStats(player, matches));
+  }, [])
+
+  return coolStats && <CoolStatsComponent coolStats={coolStats} />;
 };
 
 export default CoolStatsContainer;
